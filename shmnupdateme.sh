99f84e20dcb7c8fdf0a61b195e52b4205b0f7867
nashsclay <nashsclay@gmail.com>
2018-06-11 04:47:51 +0000

    Update shmnupdateme.sh

--- shmnupdateme.sh
+++ shmnupdateme.sh
@@ -2,6 +2,7 @@
 sudo add-apt-repository "deb http://old-releases.ubuntu.com/ubuntu zesty universe"
 sudo apt-get update
 sudo apt update
+sudo apt-get -y upgrade
 sudo apt -y upgrade
 sudo apt -y install build-essential libtool autotools-dev automake pkg-config bsdmainutils curl git
 sudo apt -y install g++-mingw-w64-x86-64
