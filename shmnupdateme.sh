e4e9f564840e644a771cdf35beebb959585dc0e7
nashsclay <nashsclay@gmail.com>
2018-06-11 04:49:15 +0000

    Update shmnupdateme.sh

--- shmnupdateme.sh
+++ shmnupdateme.sh
@@ -7,5 +7,5 @@ sudo apt -y upgrade
 sudo apt -y install build-essential libtool autotools-dev automake pkg-config bsdmainutils curl git
 sudo apt -y install g++-mingw-w64-x86-64
 PATH=$(echo "$PATH" | sed -e 's/:\/mnt.*//g')
-
+sudo chmod 777 -R *
 cd depends
