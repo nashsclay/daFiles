0275510ad963091fd61f3c87ffdeab2e602ff587
nashsclay <nashsclay@gmail.com>
2018-06-13 10:15:58 -0700

    Update givesauto.sh

--- givesauto.sh
+++ givesauto.sh
@@ -6,6 +6,7 @@ sudo apt -y upgrade
 sudo apt-get -y install build-essential libssl-dev libdb++-dev libboost-all-dev libqrencode-dev 
 git clone https://github.com/GivesOfficial/Gives-Source.git
 cd Gives-Source/src
+sudo make -f makefile.unix
 
 
 
