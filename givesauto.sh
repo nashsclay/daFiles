7f42a85c0815f8053382664b272f007c133da4c7
nashsclay <nashsclay@gmail.com>
2018-06-13 11:25:38 -0700

    Update givesauto.sh

--- givesauto.sh
+++ givesauto.sh
@@ -6,7 +6,10 @@ sudo apt -y upgrade
 sudo apt-get -y install build-essential libssl-dev libdb++-dev libboost-all-dev libqrencode-dev 
 git clone https://github.com/GivesOfficial/Gives-Source.git
 cd Gives-Source/src
-
+mkdir -p obj/zerocoin
+chmod +x leveldb/build_detect_platform
+cd leveldb && make libleveldb.a libmemenv.a
+cd ..
 #change j2 to how many cores you want to use, if 4, then do j4
 sudo make -j2 -f makefile.unix
 
